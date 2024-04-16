0202c68723ca04411851664f55f4278897592296
Vladyslav Shvaitser <vladyslav.shvaitser@planetwatchers.com>
2024-04-16 18:04:15 +0300

    Optional Gemini API key

--- internals/app.py
+++ internals/app.py
@@ -1,3 +1,4 @@
+import os
 import json
 from .tools.get_jira_ticket import get_jira_ticket_description
 from .tools.github_integration import get_last_closed_pull_requests
@@ -12,8 +13,13 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--gh_repo", type=str, required=True, help="Github repository in format owner/repository")
     parser.add_argument("--prs", type=str, default="10", help="Number of PRs to process")
+    parser.add_argument("--gemini_key", type=str, help="Gemini API key")
 
     args = parser.parse_args()
+
+    if args.gemini_key:
+        os.environ["GEMINI_KEY"] = args.gemini_key
+
     github_repo = args.gh_repo
 
     repo_name = github_repo.split("/")[1]
