af7c7d5ed31e40cb7ab0654d990ca418b0b1811e
Vladyslav Shvaitser <vladyslav.shvaitser@planetwatchers.com>
2024-04-16 16:39:18 +0300

    Small refactor

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ import json
 from tools.get_jira_ticket import get_jira_ticket_description
 from tools.github_integration import get_last_closed_pull_requests
 from tools.templates import (
-    PROMPT_START_TEMPLATE,
-    NEW_PROMPT_START_TEMPLATE,
-    ADDITIONAL_QUESTION
+    PROMPT_START_TEMPLATE
 )
 from tools.gemini_ai import get_gemini_model
 from tools.extract_jira_ticket_from_pr import extract_jira_ticket_from_pr_title
@@ -19,7 +17,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     github_repo = args.git_repository
-    # github_repo = "PlanetWatchers/cerberus"
+
     repo_name = github_repo.split("/")[1]
 
     repository_prs = get_last_closed_pull_requests(github_repo)
@@ -34,7 +32,7 @@ if __name__ == "__main__":
 
         pr = {"project_name": repo_name,"link_to_pr": f"https://github.com/{github_repo}/pull/{pr_number}", **pr}
         json_string_pr = json.dumps(pr)
-        full_prompt = NEW_PROMPT_START_TEMPLATE + json_string_pr + ADDITIONAL_QUESTION
+        full_prompt = PROMPT_START_TEMPLATE + json_string_pr
         answer = gemini_model.generate_content(full_prompt).text
 
         ai_answers.append(answer)
